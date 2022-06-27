9a55ca9e65dc8f658ba8dff1e3dc2fb4fdd99185
Fujii Masao <fujii@postgresql.org>
2022-06-27 09:47:07 +0900

    Make the test more stable.
    The content of NOTICE message about WAL archiving that pg_stop_backup()
    emits can vary depending on the WAL archiving configuration in the server.
    So this commit suppresses such a NOTICE message to make the test more stable.

--- sql/pg_exclusive_backup.sql
+++ sql/pg_exclusive_backup.sql
@@ -2,5 +2,12 @@ CREATE EXTENSION pg_exclusive_backup;
 
 SELECT 1 FROM pg_start_backup('test', true);
 SELECT pg_is_in_backup();
+
+-- Suppress NOTICE message about WAL archiving that pg_stop_backup()
+-- emits because its content can vary depending on WAL archiving
+-- configuration in the server.
+SET client_min_messages TO 'warning';
 SELECT 1 FROM pg_stop_backup();
+RESET client_min_messages;
+
 SELECT pg_is_in_backup();
